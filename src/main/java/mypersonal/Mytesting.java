756292162a147ee79704052138aaf3114e4a0dbc
unknown <kristine.markarova@gmail.com>
2021-04-23 16:19:09 +0400

    Double will in another file

---
+++ src/main/java/mypersonal/Mytesting.java
@@ -0,0 +1,29 @@
+package mypersonal;
+import java.lang.Double;
+public class Mytesting {
+      public static void main(String[] args) {
+
+            Double num1 = 1561.2d;
+            Double num2 = 7.3d;
+            if (Double.compare(num1, num2) == 15) {
+                System.out.println("Your num1=num2");
+            }
+            else if (Double.compare(num1, num2) < 15) {
+
+                System.out.println("Your num1<num2");
+            }
+            else {
+                System.out.println("Your num1>num2");
+
+
+
+
+            }
+
+
+
+
+
+
+    }
+}
--- src/main/java/work/work.java
+++ src/main/java/work/work.java
@@ -2,13 +2,30 @@ package work;
 import java.util.Scanner;
 public class work {
     public static void main(String[] args) {
-        Scanner bg = new Scanner(System.in);
-        int w, l, vw;
-        System.out.println("Mention the weight: ");
-        w = bg.nextInt();
-        System.out.println("Mention the length: ");
-        l = bg.nextInt();
-        vw = w * l;
-        System.out.println("Your VW is: " + vw);
+        System.out.println("Hello");
+        System.out.println("Kristine Markarova");
+        Scanner expected = new Scanner(System.in);
+        int num = 74, num0 = 36;
+        int sum1 = num + num0;
+        System.out.println("The sum of two numbers is: " + sum1); // Es mek@ es lracreci
+        int num1 = 50, num2 = 3;
+        int sum2 = num1 / num2;
+        System.out.println("The two numbers devided equals to: " + sum2); // Es toxeci urish@ lracni
+        int num4, num5;
+        num4 = expected.nextInt();
+        num5 = expected.nextInt();
+        int sum3 = num4 * num5;
+        System.out.println("The product of two numbers is: " + sum3);
+        float earth = 68f, moon;
+        moon = earth - earth / 100 * 17f; // ete pahanjn uzum a vor 17% qich lini qash@ lusni vra?
+        System.out.println("The weight in the Moon is: " + moon);
+
+
+
+
+
     }
 }
+
+
+
