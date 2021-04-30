4d243835307c0ddccfaff7b5689e1945500a03fd
unknown <kristine.markarova@gmail.com>
2021-04-30 18:28:01 +0400

    Missing 2 and 3

--- src/main/java/newoneforme.java
+++ src/main/java/newoneforme.java
@@ -1,12 +1,38 @@
+import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.*;
+import java.util.List;
+import java.util.stream.*;
+import java.util.function.Function;
 public class newoneforme {
     public static void main(String[] args) {
-        int x,y;
-        y= 20;
-        for (x=0; x<20; x++);
-        { System.out.println("the answer is " + x);
-            System.out.println("the answer is " + y);
-            y=y-2;
+        int y = 11;
+        int f = 11;
+        int firstone;
+        int secondone;
+        boolean thirdone;
 
-        }
+        firstone = countIt(y);
+        System.out.print("It's " + firstone);
+        secondone = addThat(y, f);
+        System.out.print("\nIt's " + secondone);
+        thirdone = numEk(y, f);
+        System.out.print("\nIt's " + thirdone);
     }
-}
+    public static int countIt(int x)
+    {
+        return(x * 100);
+    }
+    public static int addThat(int a, int b)
+    {
+        return (a-b);
+   }
+
+        public static boolean numEk(int ff, int zz){
+        if (ff == zz) return true;
+        else return false;}
+
+
+    }
+
--- src/main/java/work/work.java
+++ src/main/java/work/work.java
@@ -1,91 +1,120 @@
 package work;
 import java.util.Scanner;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.*;
+import java.util.List;
+import java.util.stream.*;
+import java.util.function.Function;
 public class work {
-    public static void main(String[] args) {
-//1. Write a program from the user input 2 numbers and 1 math operation
-// and print the output, int, double, float.
-        int num1 = 15;
-        float num2 = 12.3f;
-        double num3 = 45.6893;
-        double sum = num1 * num2 * num3;
-        System.out.println("The answere is: " + sum);
-//2. Write a program from the user input to print is the number even or not
-        Scanner numeral = new Scanner(System.in);
-        System.out.print("Please enter your number: ");
-        int number = numeral.nextInt();
-        if (number % 2 == 0)
-            System.out.println("Your number is even");
-        else
-            System.out.println("Your number is odd");
-//3. Take three numbers from the user and print the greatest number.
-        Scanner newnum = new Scanner(System.in);
-        System.out.print("Please enter the first number: ");
-        int newnum1 = numeral.nextInt();
-        System.out.print("Please enter the second number: ");
-        int newnum2 = numeral.nextInt();
-        System.out.print("Please enter the tihrd number: ");
-        int newnum3 = numeral.nextInt();
 
-        if (newnum1 > newnum2 && newnum1 > newnum3)
-            System.out.println("The greatest number is: " + newnum1);
-        else if (newnum2 > newnum1 && newnum2 > newnum3)
-            System.out.println("The greatest number is: " + newnum2);
-        else
-            System.out.println("The greatest number is: " + newnum3);
-//4. Write a Java program to get a number from the user and print whether it is positive or negative
-        Scanner x = new Scanner(System.in);
-        System.out.print("Please enter your number: ");
-        int xx = x.nextInt();
 
-        if (xx > 0)
-            System.out.println("Your number is positive: " + xx);
-        else
-            System.out.println("Your number is negative: " + xx);
-//5. Write a program from your input to print the day of the week for example 1 = Monday
-        Scanner weekdays = new Scanner(System.in);
-        System.out.print("Write the number of the day needed: ");
-        int days = weekdays.nextInt();
-
-        switch (days) {
-            case 1:
-                System.out.println("Today is Monday: ");
-                break;
-            case 2:
-                System.out.println("Today is Tuesday: ");
-                break;
-            case 3:
-                System.out.println("Today is Wednesday: ");
-                break;
-            case 4:
-                System.out.println("Today is Thursday: ");
-                break;
-            case 5:
-                System.out.println("Today is Friday: ");
-                break;
-            case 6:
-                System.out.println("Today is Saturday: ");
-                break;
-            case 7:
-                System.out.println("Today is Sunday: ");
-                break;
-            default:
-                System.out.println("No such a day: ");
-//Flowcharts
-                Scanner calcul = new Scanner(System.in);
-                System.out.println("Start");
-                System.out.print("Please enter xxx number: ");
-                int xxx = calcul.nextInt();
-                while (xxx >= 10) {
-                    xxx -= 5;
-                    System.out.println(xxx);
-                    System.out.println("Do task");
-                    System.out.println("Increment xxx");
-                }
-                if (xxx < 10)
-                System.out.println("End");
-
+//
+//        1. Գրել ծրագիր, որը կհաշվի double տիպի 10 չափանի զանգվածի էլեմենտների միջին
+//թվաբանականը;
+//         public static void main(String[] args) {
+//        double[] average = new double[] {11.1, 12.3, 9.8885546};
+//        double sum = 0;
+//        if (average.length > 0) {
+//            for (int x = 0; x < average.length; x++) {
+//                sum+=average[x];
+//                System.out.println(average[x]);
+//            }
+//            System.out.println("Sum is: " + sum);
+//            System.out.println("Average is: " + sum/average.length);
         }
+////     1. Գրել ծրագիր, որը կհաշվի double տիպի 10 չափանի զանգվածի էլեմենտների միջին
+////թվաբանականը;
+//        public static void main(String[] args) {
+//        double[] av = new double[] {11.1, 12.3, 9.8885546};
+//        double total = 0;
+//        total = av[0] + av[1]+ av[2];
+//        System.out.println("The total is: "+ total);
+//        System.out.println("The average is: "+ total/3);}
+
+//      2. Վազքի մարաթոնին մասնակցել են մի խումբ մարդիկ, ովքեր անցել են տարածությունը
+//որոշակի ժամանակում; Գտնել այն մարդու անունը ով վազել է ամենաարագը; Գտնել նաեւ
+//երկրորդ տեղը գրաված մասնակցին;
 
 
+//        3 Գրել ծրագիր, որը մուտքագրված թիվը կվերածի զանգվածի՝
+//Օրինակ՝ 567 -> [5,6,7], 23 -> [2,3]
+
+//???
+//        4 Գրել ծրագիր, որը զանգվածից կառանձնացնի բոլոր պարզ, զույգ եւ կենտ թվերը եւ կգցի առանձին զանգվածի մեջ;
+//???
+// public static void printArray(int[] evod)
+//{
+//    for (int x = 0; x < evod.length; x++)
+//        System.out.print(evod[x] + "");
+//    System.out.println();
+//}
+//    public static void main(String[] args) {
+//        int e = 5;
+//        int evod[] = {12, 45, 11, 3658, 1};
+//        int even = 0;
+//        int odd = 0;
+//        for (int x = 0; x < e; x++){
+//            if (evod[x] % 2 == 0)
+//                even++;
+//            else
+//                odd++;}
+//        int[] even2 = new int[even];
+//        int[] odd2 = new int[odd];
+//        int a = 0, b = 0;
+//        for (int x = 0; x <e; x++) {
+//            if (evod[x] % 2 == 0)
+//                even2[a++] = evod[x];
+//            else
+//                odd2[b++] = evod[x];
+//        }
+//        System.out.print("The evens are:  " );
+//        printArray(even2);
+//        System.out.print("The odds are:  " );
+//        printArray(odd2);
+//
+//    }
+//5.Գրել ծրագիր, որը կստուգի պարունակում է արդյո՞ք զանգվածը մուտքագրված թիվը;
+//
+//    public static void main(String[] args) {
+//        int[] special = {12, 11, 367, 666, 1234, 5, 77, 64};
+//        int x;
+//        Scanner num = new Scanner(System.in);
+//        System.out.print("Enter your number: ");
+//        x = num.nextInt();
+//        boolean numeral = false;
+//        for (int y : special) {
+//            if (y == x) {
+//                numeral = true;
+//                break;
+//            }
+//        }
+//        if(numeral)
+//            System.out.println("Number " + x + " is found.");
+//        else
+//            System.out.println("Number " + x + " is not found.");
+//6. Գրել ծրագիր, որը կհաշվի զանգվածի max եւ min արժեքները;
+//    public static void main(String[] args)
+//    { Integer[] num = {12, 36, 15, 44};
+//        int min = Collections.min(Arrays.asList(num));
+//        int max = Collections.max(Arrays.asList(num));
+//        System.out.println("Your minimum numeral is: " + min);
+//        System.out.println("Your maximum numeral is: " + max);
+///
     }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
