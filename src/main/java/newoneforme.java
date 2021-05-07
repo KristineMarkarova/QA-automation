eda0a4b1c7f4e6707c830cfec53d5916887932f7
unknown <kristine.markarova@gmail.com>
2021-05-07 16:07:23 +0400

    05.07.2021

--- src/main/java/newoneforme.java
+++ src/main/java/newoneforme.java
@@ -1,38 +1,46 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.*;
 import java.util.List;
+
 import java.util.stream.*;
 import java.util.function.Function;
+
 public class newoneforme {
     public static void main(String[] args) {
-        int y = 11;
-        int f = 11;
-        int firstone;
-        int secondone;
-        boolean thirdone;
-
-        firstone = countIt(y);
-        System.out.print("It's " + firstone);
-        secondone = addThat(y, f);
-        System.out.print("\nIt's " + secondone);
-        thirdone = numEk(y, f);
-        System.out.print("\nIt's " + thirdone);
-    }
-    public static int countIt(int x)
-    {
-        return(x * 100);
-    }
-    public static int addThat(int a, int b)
-    {
-        return (a-b);
-   }
-
-        public static boolean numEk(int ff, int zz){
-        if (ff == zz) return true;
-        else return false;}
+        int[] number = {15, 44, 45, 156};
+        int random = (int) (Math.random()*15000);
+        System.out.println(random);
+//        System.out.println(Arrays.toString(number));
+//        System.out.println(number.length);
+//        int start;
+//        int x;
+//        for (x = 0; x<number.length; x++)
+//        if (number[x] > number.length) {
+//            System.out.println(number[x]);
+//            int max;
+//            int max = Collections.min(Arrays.asList(number));
+//        String fa = "aads";
+//        System.out.println(fa.toUpperCase(Locale.ROOT));
 
 
-    }
+
+
+        }
+
+
+            }
+
+
+
+
+
+
+
+
+
+
+
 
--- src/main/java/work/work.java
+++ src/main/java/work/work.java
@@ -1,107 +1,74 @@
 package work;
-import java.util.Scanner;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.*;
-import java.util.List;
-import java.util.stream.*;
-import java.util.function.Function;
+
 public class work {
-
-
-//
-//        1. Գրել ծրագիր, որը կհաշվի double տիպի 10 չափանի զանգվածի էլեմենտների միջին
-//թվաբանականը;
-//         public static void main(String[] args) {
-//        double[] average = new double[] {11.1, 12.3, 9.8885546};
-//        double sum = 0;
-//        if (average.length > 0) {
-//            for (int x = 0; x < average.length; x++) {
-//                sum+=average[x];
-//                System.out.println(average[x]);
+        public static void main(String[] args) {
+//1.
+//            int line = 5;
+//            for (int i = 1; i <=line; i ++)
+//            {
+//                for (int j = 1; j < i+1; j ++)
+//                {
+//                    System.out.print(j + " ");
+//                    }
+//                System.out.println();
 //            }
-//            System.out.println("Sum is: " + sum);
-//            System.out.println("Average is: " + sum/average.length);
+
+
+//2.
+//            for (int i = 6; i>0; i -- )
+//            {
+//                for (int j = 0; j< i-1; j ++){
+//                    System.out.print("* ");
+//                }System.out.println();
+//            };
+
+//3.
+//        int line = 1;
+//        for (int i = 5; i >= line; i --) {
+//            for (int j = 1; j <= i; j++)
+//            {
+//                System.out.print( j + " ");
+//            }
+//            System.out.println();
+//        }
+//
+//4.
+//        int a = 5, b = 0;
+//        for (int i = 1; i <= a; ++i, b =0)
+//        {
+//            for (int j = 1; j <= a - i; ++j)
+//            {
+//                System.out.print("   ");
+//            }
+//            while (b != 2 * i - 1) {
+//                System.out.print(" * ");
+//                ++b; }
+//            System.out.println();
+//        }
+//5.
+//         int line = 0, num = 10;
+//         for (int i = 5; i > line; --i)
+//         {
+//             for (int j = 1; j <= i-1; ++j)
+//             {
+//                 System.out.print(num + " ");
+//                 --num;
+//             }
+//             System.out.println();
+//         }
+//6.
+
+
+
+
+
+
+
+
+
         }
-////     1. Գրել ծրագիր, որը կհաշվի double տիպի 10 չափանի զանգվածի էլեմենտների միջին
-////թվաբանականը;
-//        public static void main(String[] args) {
-//        double[] av = new double[] {11.1, 12.3, 9.8885546};
-//        double total = 0;
-//        total = av[0] + av[1]+ av[2];
-//        System.out.println("The total is: "+ total);
-//        System.out.println("The average is: "+ total/3);}
+}
 
-//      2. Վազքի մարաթոնին մասնակցել են մի խումբ մարդիկ, ովքեր անցել են տարածությունը
-//որոշակի ժամանակում; Գտնել այն մարդու անունը ով վազել է ամենաարագը; Գտնել նաեւ
-//երկրորդ տեղը գրաված մասնակցին;
-
-
-//        3 Գրել ծրագիր, որը մուտքագրված թիվը կվերածի զանգվածի՝
-//Օրինակ՝ 567 -> [5,6,7], 23 -> [2,3]
-
-//???
-//        4 Գրել ծրագիր, որը զանգվածից կառանձնացնի բոլոր պարզ, զույգ եւ կենտ թվերը եւ կգցի առանձին զանգվածի մեջ;
-//???
-// public static void printArray(int[] evod)
-//{
-//    for (int x = 0; x < evod.length; x++)
-//        System.out.print(evod[x] + "");
-//    System.out.println();
-//}
-//    public static void main(String[] args) {
-//        int e = 5;
-//        int evod[] = {12, 45, 11, 3658, 1};
-//        int even = 0;
-//        int odd = 0;
-//        for (int x = 0; x < e; x++){
-//            if (evod[x] % 2 == 0)
-//                even++;
-//            else
-//                odd++;}
-//        int[] even2 = new int[even];
-//        int[] odd2 = new int[odd];
-//        int a = 0, b = 0;
-//        for (int x = 0; x <e; x++) {
-//            if (evod[x] % 2 == 0)
-//                even2[a++] = evod[x];
-//            else
-//                odd2[b++] = evod[x];
-//        }
-//        System.out.print("The evens are:  " );
-//        printArray(even2);
-//        System.out.print("The odds are:  " );
-//        printArray(odd2);
-//
-//    }
-//5.Գրել ծրագիր, որը կստուգի պարունակում է արդյո՞ք զանգվածը մուտքագրված թիվը;
-//
-//    public static void main(String[] args) {
-//        int[] special = {12, 11, 367, 666, 1234, 5, 77, 64};
-//        int x;
-//        Scanner num = new Scanner(System.in);
-//        System.out.print("Enter your number: ");
-//        x = num.nextInt();
-//        boolean numeral = false;
-//        for (int y : special) {
-//            if (y == x) {
-//                numeral = true;
-//                break;
-//            }
-//        }
-//        if(numeral)
-//            System.out.println("Number " + x + " is found.");
-//        else
-//            System.out.println("Number " + x + " is not found.");
-//6. Գրել ծրագիր, որը կհաշվի զանգվածի max եւ min արժեքները;
-//    public static void main(String[] args)
-//    { Integer[] num = {12, 36, 15, 44};
-//        int min = Collections.min(Arrays.asList(num));
-//        int max = Collections.max(Arrays.asList(num));
-//        System.out.println("Your minimum numeral is: " + min);
-//        System.out.println("Your maximum numeral is: " + max);
-///
-    }
 
 
 
